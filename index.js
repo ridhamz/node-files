1ccf33fe5ede5cf1c8b6e2aeac25683359516951
ridha mezrigui <ridhamezrigui07@gmail.com>
2022-02-05 18:58:52 +0100

    delete testing files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { getFileData, writeFileData, clearFileData, updateFileData } from "./dataOperations.js";
+import { getFileData, writeFileData, clearFileData, updateFileData } from "./lib/dataOperations.js";
 
-import { createFile, deleteFile } from "./fileOperations.js";
+import { createFile, deleteFile } from "./lib/fileOperations.js";
 
 
 // export data operations functions
